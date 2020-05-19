c8ebc3766dea447e0ab42401d02d3852761a0cec
Yuar <Yuar@DESKTOP-8U71I7Q>
2020-05-20 02:12:18 +0900

    05/20

---
+++ anime/main.py
@@ -0,0 +1,45 @@
+import requests
+import re
+from bs4 import BeautifulSoup as bs
+from datetime import datetime
+
+
+r = requests.get('https://ani.gamer.com.tw/')
+source = bs(r.text,'html.parser')
+def newani(n,m):
+    try:
+       
+        soup = source.find_all(class_='newanime-title')
+        title=soup[n].text.strip()
+       
+        soup = source.find_all(class_='newanime-date')
+        date = soup[n].text.strip()
+        day = re.split('/',date)
+        day=re.split(' ',day[1])
+        
+        soup = source.find_all(class_='newanime-vol')
+        vol = soup[n].text.strip()
+     
+        soup=source.find_all(class_="newanime__content__cover")
+        src = str(soup[n])
+        src = src.split('"')[5]
+
+        if(day[0]==str(datetime.now().day)):
+               result = str("<span>"+date+vol+"</span>"+"<h4>"+title+"</h4>")
+               srcresult=src
+        else:
+             srcresult = "https://i.imgur.com/2FYaJGi.jpg"
+     
+        if(m==0):    
+             return result
+        else:
+             return srcresult
+       
+    except :
+        return "本日無更新"
+
+
+
+print(newani(0,1))
+    
+
---
+++ anime/views.py
@@ -0,0 +1,49 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from anime import app
+import requests
+from bs4 import BeautifulSoup as bs
+from anime.main import newani
+from flask import jsonify
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+@app.route('/new')
+def new():
+    return jsonify(result0=newani(0,0),img0=newani(0,1),
+                              result1=newani(1,0),img1=newani(1,1),
+                              result2=newani(2,0),img2=newani(2,1),
+                              result3=newani(3,0),img3=newani(3,1))
+
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
